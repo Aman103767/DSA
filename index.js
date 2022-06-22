cb5a1bff098b50de945458189df3d17bd8575406
Aman103767 <avinash103767@gmail.com>
2022-06-23 00:57:22 +0530

    recursion practice

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-var arr =[39,27,11,4,24,32,32,1];
-var top = -1;
-var stk = [];
-var ans = []
-for(var i= 0;i<arr.length;i++){
-while(top != -1&&stk[top]<= arr[i]){
-     stk.pop();
-     top--;
-}if(top == -1){
-    ans.push(-1)
-}else{
-    ans.push(stk[top])
+function print(n){
+    if(n==1){
+        return 1;
+    }
+  
+    return n*print(n-1);
 }
-top++
-stk.push(arr[i]);
-
-}
-console.log(stk)
+console.log(print(5));
